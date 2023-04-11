5bb6c3580b4304ce1c32c41a6a20cbc6f30cac43
aleksandrkomyagin <112546606+aleksandrkomyagin@users.noreply.github.com>
2023-04-11 13:26:27 +0300

    Update serializers.py

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -7,10 +7,10 @@ User = get_user_model()
 
 
 class CreateUserConfirmationCode(serializers.Serializer):
-    username = serializers.CharField(max_length=200, required=True)
+    username = serializers.CharField(max_length=150, required=True)
     email = serializers.EmailField(required=True)
 
 
 class CreateUserToken(serializers.Serializer):
-    username = serializers.CharField(max_length=200, required=True)
-    confirmation_code = serializers.CharField(max_length=200, required=True)
+    username = serializers.CharField(max_length=150, required=True)
+    confirmation_code = serializers.CharField(max_length=150, required=True)
