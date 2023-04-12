b39fe3ce3f01c8b277ed36ab0587efde8bc68356
aleksandrkomyagin <aleksandrkomyagin8@yandex.ru>
2023-04-12 10:15:18 +0300

    Исправление в файле api/urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -6,6 +6,6 @@ from api.views import get_confirmation_code, get_token_view
 app_name = 'api'
 
 urlpatterns = [
-    path('v1/registration/', get_confirmation_code, name='registration'),
-    path('v1/token/', get_token_view, name='token'),
+    path('v1/auth/signup/', get_confirmation_code, name='registration'),
+    path('v1/auth/token/', get_token_view, name='token'),
 ]
