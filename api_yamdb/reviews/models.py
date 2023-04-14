80fc2877f7df37fa0a01f1a96d3c00437f483b5f
aleksandrkomyagin <aleksandrkomyagin8@yandex.ru>
2023-04-14 20:31:39 +0300

    Перенос модели User в отдельное приложение

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,90 +1,9 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.contrib.auth import get_user_model
 
-# from .validators import validate_username, validate_year
 from .validators import validate_year
 
-# USER = 'user'
-# ADMIN = 'admin'
-# MODERATOR = 'moderator'
-
-# ROLE_CHOICES = [
-#     (USER, USER),
-#     (ADMIN, ADMIN),
-#     (MODERATOR, MODERATOR),
-# ]
-
-
-# class User(AbstractUser):
-#     username = models.CharField(
-#         validators=(validate_username,),
-#         max_length=150,
-#         unique=True,
-#         blank=False,
-#         null=False
-#     )
-#     email = models.EmailField(
-#         max_length=254,
-#         unique=True,
-#         blank=False,
-#         null=False
-#     )
-#     role = models.CharField(
-#         'роль',
-#         max_length=20,
-#         choices=ROLE_CHOICES,
-#         default=USER,
-#         blank=True
-#     )
-#     bio = models.TextField(
-#         'биография',
-#         blank=True,
-#     )
-#     first_name = models.CharField(
-#         'имя',
-#         max_length=150,
-#         blank=True
-#     )
-#     last_name = models.CharField(
-#         'фамилия',
-#         max_length=150,
-#         blank=True
-#     )
-
-#     @property
-#     def is_user(self):
-#         return self.role == USER
-
-#     @property
-#     def is_admin(self):
-#         return self.role == ADMIN
-
-#     @property
-#     def is_moderator(self):
-#         return self.role == MODERATOR
-
-#     class Meta:
-#         ordering = ('id',)
-#         verbose_name = 'Пользователь'
-#         verbose_name_plural = 'Пользователи'
-
-#     def __str__(self):
-#         return self.username
-
-
-# class EmailVerification(models.Model):
-#     code = models.UUIDField(unique=True)
-#     user = models.OneToOneField(
-#         User,
-#         on_delete=models.CASCADE,
-#         related_name='code'
-#     )
-#     created = models.DateTimeField(auto_now_add=True)
-
-#     def __str__(self):
-#         return f'EmailVerification object for {self.user.email}'
 User = get_user_model()
 
 
--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -1,26 +1,11 @@
 import datetime as dt
-import re
 
-from django.core.exceptions import ValidationError
 from rest_framework import serializers
-
-
-def validate_username(value):
-    if value == 'me':
-        raise serializers.ValidationError(
-            'Нельзя использовать юзернэйм <me>.',
-            {'value': value}
-        )
-    if re.search(r'^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$', value) is None:
-        raise serializers.ValidationError(
-            f'Не допустимые символы <{value}> в нике.',
-            {'value': value}
-        )
 
 
 def validate_year(value):
     if dt.date.today().year < value:
-        raise ValidationError(
+        raise serializers.ValidationError(
             'Год создания не может быть больше текущего'
         )
     return value
